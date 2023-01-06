ad5acca886edecbf1f4771e07d2164f1322fc3fa
leoaam24 <102619114+leoaam24@users.noreply.github.com>
2023-01-06 11:32:16 +0800

    Finished Prep 2 Program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter;
+
+        Console.Write("Please enter your Grade: ");
+        string studentGrades = Console.ReadLine();
+
+        
+
+        float studentGrade = float.Parse(studentGrades);
+
+        if (studentGrade >= 60 && studentGrade < 70) {letter = "D";}
+        else if (studentGrade >= 70 && studentGrade < 80) {letter = "C";}
+        else if (studentGrade >= 80 && studentGrade < 90) {letter = "B";}
+        else if (studentGrade >= 90) {letter ="A";}
+        else {letter = "F";}
+
+        if ((studentGrade % 10) >= 7 && !(studentGrade !> 96) && !(studentGrade !< 60))
+        {
+            Console.WriteLine($"{letter}+");
+        }
+
+        else if((studentGrade % 10) < 3 && !(studentGrade !<60))
+        {
+            Console.WriteLine($"{letter}-");
+        }
+
+        else
+        {
+            Console.WriteLine(letter);
+        }
+
+
+        if (studentGrade > 70)
+        {
+            Console.WriteLine("You passed the class.");
+        }
+
+        else {
+            Console.WriteLine("You failed please try again.");
+        }
+
     }
 }
