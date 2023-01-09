db11438bd20fefdd48af3b3e59c258455b606218
leoaam24 <102619114+leoaam24@users.noreply.github.com>
2023-01-09 15:19:00 +0800

    Finished Prep 3 Program.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        
+
+        string startGuess = "no";
+        do {
+            int number = randomGenerator.Next(1, 101);
+            Console.Write("Do you want to start Guessing?(yes/no) ");
+            startGuess = Console.ReadLine();
+            string statusGuess;
+            if (startGuess == "yes") 
+            {
+                statusGuess = "continue";
+            }
+            else {
+                statusGuess = "stop";
+            }
+                while (statusGuess == "continue")
+                    {
+
+                    int numberTries = 0;
+
+                        do
+                        {
+                            Console.Write("What is your guess? ");
+                            string _guessNumber = Console.ReadLine();
+
+                            int guessNumber = int.Parse(_guessNumber);
+
+                            if (guessNumber > number) 
+                            {
+                                Console.WriteLine("Lower");
+                                statusGuess = "continue";
+                                numberTries += 1;
+
+                            }
+                    
+                            else if (guessNumber < number)
+                            {
+                                Console.WriteLine("Higher");
+                                statusGuess = "continue";
+                                numberTries += 1;
+                            }
+                            else 
+                            {   
+                                numberTries += 1;
+                                Console.WriteLine("You guessed it!");
+                                Console.WriteLine($"You've Guessed it {numberTries} time/s.");
+                                statusGuess = "stop";
+                                startGuess = "yes";
+
+                            }
+                        } while (statusGuess == "continue");
+                    }
+            } while (startGuess == "yes");
+        if (startGuess ==  "no")
+    {
+        Console.WriteLine("Comeback when you're ready.");
     }
+    }   
+    
 }
