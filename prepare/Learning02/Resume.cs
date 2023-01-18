ac9e61cc5a86ca9ac90b26a2662dd4706a8a6ace
leoaam24 <102619114+leoaam24@users.noreply.github.com>
2023-01-18 15:08:02 +0800

    Finished Learning 02 Program

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+public class Resume
+{
+    //member variable
+    public string _name = "";
+    public List<Job> _jobs = new List<Job>();
+
+    public Resume()
+    {
+    }
+
+    public void DisplayResume()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine($"Jobs: ");
+        foreach (Job job in _jobs) 
+        {
+            job.Display();
+        }
+    }
+
+}
