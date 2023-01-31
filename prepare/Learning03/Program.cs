f47d839ee0f4c51cf7e777978d310ad86c0c8256
leoaam24 <102619114+leoaam24@users.noreply.github.com>
2023-01-31 14:05:47 +0800

    Finished Learning 3 prepare Fraction

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction firstConstructor  = new Fraction();
+        int numerator = firstConstructor.GetTop();
+        int denominator = firstConstructor.GetBottom();
+        // numerator = firstConstructor.SetTop(4);
+        // denominator = firstConstructor.SetBottom(2);
+        Console.WriteLine(firstConstructor.GetFractionString());
+        Console.WriteLine(firstConstructor.GetDecimalValue());
+
+        Fraction secondConstructor = new Fraction(5);
+        int c2Numerator = secondConstructor.GetTop();
+        int c2Denominator = secondConstructor.GetBottom();
+        // c2Numerator = secondConstructor.SetTop(8);
+        // c2Denominator = secondConstructor.SetBottom(4);
+        Console.WriteLine(secondConstructor.GetFractionString());
+        Console.WriteLine(secondConstructor.GetDecimalValue());
+
+        Fraction thirdConstructor = new Fraction(1,3);
+        int c3Numerator = thirdConstructor.GetTop();
+        int c3Denominator = thirdConstructor.GetBottom();
+        // c3Numerator = thirdConstructor.SetTop(6);
+        // c3Denominator = thirdConstructor.SetBottom(2);
+        Console.WriteLine(thirdConstructor.GetFractionString());
+        Console.WriteLine(thirdConstructor.GetDecimalValue());
+
+
+        
     }
 }
