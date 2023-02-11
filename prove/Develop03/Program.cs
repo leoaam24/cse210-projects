0f27be024ec777242d55a7898d1b6e6bac669c44
leoaam24 <102619114+leoaam24@users.noreply.github.com>
2023-02-12 07:25:57 +0800

    finished work for develop03 program

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,29 +5,28 @@ class Program
     static void Main(string[] args)
     {
         
-        ScriptureReferrence SR = new ScriptureReferrence("John 3:5");
+        ScriptureReferrence SR = new ScriptureReferrence();
         ScriptureText ST = new ScriptureText();
         HideWords HW = new HideWords(ST.getScriptureList());
         string UserInput = "start";
-
-
-
-
-        string selectedScripture = "Jesus answered, Verily, verily, I say unto thee, Except a man be born of water and of the Spirit, he cannot enter into the kingdom of God.";
-        ST.setScriptureText(selectedScripture);
+        // string selectedScripture = "Jesus answered, Verily, verily, I say unto thee, Except a man be born of water and of the Spirit, he cannot enter into the kingdom of God.";
+        // ST.setScriptureText(selectedScripture);
+        ST.setScriptureText(SR.getSRText());
         Console.Write($"{SR.getSR()}");
         ST.displayScriptureText();
         Console.WriteLine();
         Console.WriteLine();
         Console.WriteLine("Press enter to continue or type 'quit' to finish:");
+        UserInput = Console.ReadLine();
         HW.createStoredIndexList();
         int loopStopIndicator = HW.stopLoop();
         while (UserInput != "quit")
         { 
-            UserInput = Console.ReadLine();
             Console.Clear();
             Console.Write($"{SR.getSR()}");
             HW.replaceWords();
+            Console.WriteLine();
+            Console.WriteLine("Press enter to continue or type 'quit' to finish:");
             UserInput = Console.ReadLine();
             loopStopIndicator = HW.stopLoop();
             if (loopStopIndicator == 1)
--- prove/Develop03/ScriptureReferrence.cs
+++ prove/Develop03/ScriptureReferrence.cs
@@ -2,8 +2,12 @@ public class ScriptureReferrence
 {
     private string _scripture = "";
 
+    private string _scriptureReferrenceText = "";
+
     public ScriptureReferrence()
     {
+        _scripture = "Matthew 5:22";
+        _scriptureReferrenceText = "But I say unto you, That whosoever is angry with his brother without a cause shall be in danger of the judgment: and whosoever shall say to his brother, Raca, shall be in danger of the council: but whosoever shall say, Thou fool, shall be in danger of hell fire.";
     }
 
     public ScriptureReferrence(string referrence)
@@ -13,6 +17,10 @@ public class ScriptureReferrence
 
     public string getSR(){
         return _scripture;
+    }
+
+    public string getSRText(){
+        return _scriptureReferrenceText;
     }
 
 }
